efc73372be97edc7a3e86dff58f94266fd6d33f2
sakthivelseenivaasan <85880284+sakthivelseenivaasan@users.noreply.github.com>
2021-06-15 13:52:31 +0530

    Add files via upload

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,66 @@
+<template>
+ <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Enter Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Enter password"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </template>
+
+<script>
+export default {
+  name: 'Login',
+data(){
+    return{
+        form:{
+            email:'',
+            password:''
+        }
+        }
+},
+  methods:{
+       onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
